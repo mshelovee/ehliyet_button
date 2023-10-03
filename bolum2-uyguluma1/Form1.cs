d483d423094b3863c69108640cb7a8c2710c3bab
hilal <130781977+mshelovee@users.noreply.github.com>
2023-10-03 14:51:27 +0300

    Add files via upload

---
+++ bolum2-uyguluma1/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bolum2_uyguluma1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMesaj_Click(object sender, EventArgs e)
+        {
+            int yas = 17;
+            // yaş 18 ve üzerindeyse "ehliyet alınabilir" yazdıran program.
+            if (yas >= 18)
+            {
+                MessageBox.Show("Ehliyet Alabilirsiniz");
+            }
+
+            if (yas <= 18)
+            {
+                MessageBox.Show("Biraz daha büyümelisin");
+            }
+        }
+}   }
